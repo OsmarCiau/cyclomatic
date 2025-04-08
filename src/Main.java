640380d422a54a04386c5c35f9a0e9ae56415a21
Osmar <osmarcc70@gmail.com>
2025-04-07 22:12:57 -0600

    test2

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,22 @@
 public class Main {
+    public static void cyclomaticTest(int a) {
+        switch (a) {
+            case 1:
+                System.out.println("one");
+                break;
+            case 2:
+                System.out.println("two");
+                break;
+            case 3:
+                System.out.println("many");
+                break;
+            default:
+                System.out.println("lots");
+                break;
+        }
+    }
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        cyclomaticTest(3);
     }
 }
